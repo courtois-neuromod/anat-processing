680d033ea28445732f6650419118e029638b7fab
Julien Cohen-Adad <jcohen@polymtl.ca>
2021-06-09 21:28:31 -0400

    Added MTS

--- process_spinalcord.sh
+++ process_spinalcord.sh
@@ -21,7 +21,7 @@
 set -x
 
 # Immediately exit if error
-set -e -o pipefail
+# set -e -o pipefail
 
 # Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
 trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
@@ -58,6 +58,7 @@ label_if_does_not_exist(){
   local file="$1"
   local file_seg="$2"
   local vertlevels="$3"
+  local contrast="$4"
   # Update global variable with segmentation file name
   FILELABEL="${file}_labels"
   FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
@@ -68,7 +69,7 @@ label_if_does_not_exist(){
   else
     echo "Not found. Proceeding with automatic labeling."
     # Generate labeled segmentation
-    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -c t1
+    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -c $contrast
     # Create labels in the cord at mid-vertebral levels
     sct_label_utils -i ${file_seg}_labeled.nii.gz -vert-body ${vertlevels} -o ${FILELABEL}.nii.gz
   fi
@@ -148,19 +149,26 @@ cd ${SUBJECT_SESSION}/anat/
 SUBJECT="${SUBJECT}_${SESSION}"
 
 
-# T2
+# T2w
 # ------------------------------------------------------------------------------
 file_t2="${SUBJECT}_bp-cspine_T2w"
 # Segment spinal cord (only if it does not exist)
 segment_if_does_not_exist $file_t2 "t2"
 file_t2_seg=$FILESEG
 # Create mid-vertebral levels in the cord (only if it does not exist)
-label_if_does_not_exist ${file_t2} ${file_t2_seg} "2,5"
+label_if_does_not_exist ${file_t2} ${file_t2_seg} "2,5" t2
 file_label=$FILELABEL
-
+# Register to PAM50 template
+sct_register_to_template -i ${file_t2}.nii.gz -s ${file_t2_seg}.nii.gz -l ${file_label}.nii.gz -c t2 -param step=1,type=seg,algo=centermassrot:step=2,type=seg,algo=syn,slicewise=1,smooth=0,iter=5:step=3,type=im,algo=syn,slicewise=1,smooth=0,iter=3 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+# Rename warping fields for clarity
+mv warp_template2anat.nii.gz warp_template2T2w.nii.gz
+mv warp_anat2template.nii.gz warp_T2w2template.nii.gz
+# Warp template without the white matter atlas (we don't need it at this point)
+sct_warp_template -d ${file_t2}.nii.gz -w warp_template2T2w.nii.gz -a 0 -ofolder label_T2w
+# Generate QC report to assess vertebral labeling
+sct_qc -i ${file_t2}.nii.gz -s label_T2w/template/PAM50_levels.nii.gz -p sct_label_vertebrae -qc ${PATH_QC} -qc-subject ${SUBJECT}
 # Compute average cord CSA between C2 and C3
-sct_process_segmentation -i ${file_t2_seg}.nii.gz -vert 2:3 -vertfile PAM50_levels2${file_t2}.nii.gz -o ${PATH_RESULTS}/csa-SC_T2w.csv -append 1
-
+sct_process_segmentation -i ${file_t2_seg}.nii.gz -vert 2:3 -vertfile ${file_t2_seg}_labeled.nii.gz -o ${PATH_RESULTS}/csa-SC_T2w.csv -append 1
 
 # T1w
 # ------------------------------------------------------------------------------
@@ -172,61 +180,52 @@ file_t1="${SUBJECT}_T1w"
 # Segment spinal cord (only if it does not exist)
 segment_if_does_not_exist $file_t1 "t1"
 file_t1_seg=$FILESEG
-# # Bring vertebral level into T2 space
-# sct_register_multimodal -i label_T1w/template/PAM50_levels.nii.gz -d ${file_t2_seg}.nii.gz -o PAM50_levels2${file_t2}.nii.gz -identity 1 -x nn
-# # Compute average cord CSA between C2 and C3
-# sct_process_segmentation -i ${file_t2_seg}.nii.gz -vert 2:3 -vertfile PAM50_levels2${file_t2}.nii.gz -o ${PATH_RESULTS}/csa-SC_T2w.csv -append 1
-# 
-# # Generate QC report to assess vertebral labeling
-# sct_qc -i ${file_t1}.nii.gz -s ${file_label} -p sct_label_utils -qc ${PATH_QC} -qc-subject ${SUBJECT}
-# # Compute average cord CSA between C2 and C3
-# sct_process_segmentation -i ${file_t1_seg}.nii.gz -vert 2:3 -vertfile label_T1w/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/csa-SC_T1w.csv -append 1
-# 
-# # MTS
-# # ------------------------------------------------------------------------------
-# file_t1w="${SUBJECT}_acq-T1w_MTS"
-# file_mton="${SUBJECT}_acq-MTon_MTS"
-# file_mtoff="${SUBJECT}_acq-MToff_MTS"
-# 
-# if [[ -e "${file_t1w}.nii.gz" && -e "${file_mton}.nii.gz" && -e "${file_mtoff}.nii.gz" ]]; then
-#   # Segment spinal cord (only if it does not exist)
-#   segment_if_does_not_exist $file_t1w "t1"
-#   file_t1w_seg=$FILESEG
-#   # Create mask
-#   sct_create_mask -i ${file_t1w}.nii.gz -p centerline,${file_t1w_seg}.nii.gz -size 35mm -o ${file_t1w}_mask.nii.gz
-#   # Crop data for faster processing
-#   sct_crop_image -i ${file_t1w}.nii.gz -m ${file_t1w}_mask.nii.gz -o ${file_t1w}_crop.nii.gz
-#   file_t1w="${file_t1w}_crop"
-#   # Register PD->T1w
-#   # Tips: here we only use rigid transformation because both images have very similar sequence parameters. We don't want to use SyN/BSplineSyN to avoid introducing spurious deformations.
-#   sct_register_multimodal -i ${file_mtoff}.nii.gz -d ${file_t1w}.nii.gz -dseg ${file_t1w_seg}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline -qc ${PATH_QC} -qc-subject ${SUBJECT}
-#   file_mtoff="${file_mtoff}_reg"
-#   # Register MT->T1w
-#   sct_register_multimodal -i ${file_mton}.nii.gz -d ${file_t1w}.nii.gz -dseg ${file_t1w_seg}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline -qc ${PATH_QC} -qc-subject ${SUBJECT}
-#   file_mton="${file_mton}_reg"
-#   # Copy json files to match file basename (it will later be used by sct_compute_mtsat)
-#   cp ${SUBJECT}_acq-T1w_MTS.json ${file_t1w}.json
-#   cp ${SUBJECT}_acq-MToff_MTS.json ${file_mtoff}.json
-#   cp ${SUBJECT}_acq-MTon_MTS.json ${file_mton}.json
-#   # Register template->T1w_ax (using template-T1w as initial transformation)
-#   sct_register_multimodal -i $SCT_DIR/data/PAM50/template/PAM50_t1.nii.gz -iseg $SCT_DIR/data/PAM50/template/PAM50_cord.nii.gz -d ${file_t1w}.nii.gz -dseg ${file_t1w_seg}.nii.gz -param step=1,type=seg,algo=slicereg,metric=MeanSquares,smooth=2:step=2,type=im,algo=syn,metric=CC,iter=5,gradStep=0.5 -initwarp warp_template2T1w.nii.gz -initwarpinv warp_T1w2template.nii.gz
-#   # Rename warping field for clarity
-#   mv warp_PAM50_t12${file_t1w}.nii.gz warp_template2axT1w.nii.gz
-#   mv warp_${file_t1w}2PAM50_t1.nii.gz warp_axT1w2template.nii.gz
-#   # Warp template
-#   sct_warp_template -d ${file_t1w}.nii.gz -w warp_template2axT1w.nii.gz -ofolder label_axT1w -qc ${PATH_QC} -qc-subject ${SUBJECT}
-#   # Compute MTR
-#   sct_compute_mtr -mt0 ${file_mtoff}.nii.gz -mt1 ${file_mton}.nii.gz
-#   # Compute MTsat
-#   sct_compute_mtsat -mt ${file_mton}.nii.gz -pd ${file_mtoff}.nii.gz -t1 ${file_t1w}.nii.gz
-#   # Extract MTR, MTsat and T1 in WM between C2 and C5 vertebral levels
-#   sct_extract_metric -i mtr.nii.gz -f label_axT1w/atlas -l 51 -vert 2:5 -vertfile label_axT1w/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/MTR.csv -append 1
-#   sct_extract_metric -i mtsat.nii.gz -f label_axT1w/atlas -l 51 -vert 2:5 -vertfile label_axT1w/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/MTsat.csv -append 1
-#   sct_extract_metric -i t1map.nii.gz -f label_axT1w/atlas -l 51 -vert 2:5 -vertfile label_axT1w/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/T1.csv -append 1
-# else
-#   echo "WARNING: MTS dataset is incomplete."
-# fi
-# 
+# Bring vertebral level into T1w space
+sct_register_multimodal -i label_T2w/template/PAM50_levels.nii.gz -d ${file_t1_seg}.nii.gz -o PAM50_levels2${file_t1}.nii.gz -identity 1 -x nn
+# Compute average cord CSA between C2 and C3
+sct_process_segmentation -i ${file_t1_seg}.nii.gz -vert 2:3 -vertfile PAM50_levels2${file_t1}.nii.gz -o ${PATH_RESULTS}/csa-SC_T1w.csv -append 1
+ 
+# MTS
+# ------------------------------------------------------------------------------
+file_t1ax="${SUBJECT}_acq-T1w_bp-cspine_MTS"
+file_mton="${SUBJECT}_acq-MTon_bp-cspine_MTS"
+file_mtoff="${SUBJECT}_acq-MToff_bp-cspine_MTS"
+
+# Segment spinal cord (only if it does not exist)
+segment_if_does_not_exist $file_t1ax "t1"
+file_t1ax_seg=$FILESEG
+# Create mask
+sct_create_mask -i ${file_t1ax}.nii.gz -p centerline,${file_t1ax_seg}.nii.gz -size 35mm -o ${file_t1ax}_mask.nii.gz
+# Crop data for faster processing
+sct_crop_image -i ${file_t1ax}.nii.gz -m ${file_t1ax}_mask.nii.gz -o ${file_t1ax}_crop.nii.gz
+file_t1ax="${file_t1ax}_crop"
+# Register PD->T1w
+# Tips: here we only use rigid transformation because both images have very similar sequence parameters. We don't want to use SyN/BSplineSyN to avoid introducing spurious deformations.
+sct_register_multimodal -i ${file_mtoff}.nii.gz -d ${file_t1ax}.nii.gz -dseg ${file_t1ax_seg}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline -qc ${PATH_QC} -qc-subject ${SUBJECT}
+file_mtoff="${file_mtoff}_reg"
+# Register MT->T1w
+sct_register_multimodal -i ${file_mton}.nii.gz -d ${file_t1ax}.nii.gz -dseg ${file_t1ax_seg}.nii.gz -param step=1,type=im,algo=rigid,slicewise=1,metric=CC -x spline -qc ${PATH_QC} -qc-subject ${SUBJECT}
+file_mton="${file_mton}_reg"
+# Copy json files to match file basename (it will later be used by sct_compute_mtsat)
+cp ${SUBJECT}_acq-T1w_bp-cspine_MTS.json ${file_t1ax}.json
+cp ${SUBJECT}_acq-MToff_bp-cspine_MTS.json ${file_mtoff}.json
+cp ${SUBJECT}_acq-MTon_bp-cspine_MTS.json ${file_mton}.json
+# Register template->T1w_ax (using template-T1w as initial transformation)
+sct_register_multimodal -i $SCT_DIR/data/PAM50/template/PAM50_t1.nii.gz -iseg $SCT_DIR/data/PAM50/template/PAM50_cord.nii.gz -d ${file_t1ax}.nii.gz -dseg ${file_t1ax_seg}.nii.gz -param step=1,type=seg,algo=slicereg,metric=MeanSquares,smooth=2:step=2,type=im,algo=syn,metric=CC,iter=5,gradStep=0.5 -initwarp warp_template2T2w.nii.gz -initwarpinv warp_T2w2template.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT}
+# Rename warping field for clarity
+mv warp_PAM50_t12${file_t1ax}.nii.gz warp_template2T1ax.nii.gz
+mv warp_${file_t1ax}2PAM50_t1.nii.gz warp_T1ax2template.nii.gz
+# Warp template
+sct_warp_template -d ${file_t1ax}.nii.gz -w warp_template2T1ax.nii.gz -ofolder label_MTS -qc ${PATH_QC} -qc-subject ${SUBJECT}
+# Compute MTR
+sct_compute_mtr -mt0 ${file_mtoff}.nii.gz -mt1 ${file_mton}.nii.gz
+# Compute MTsat
+sct_compute_mtsat -mt ${file_mton}.nii.gz -pd ${file_mtoff}.nii.gz -t1 ${file_t1ax}.nii.gz
+# Extract MTR, MTsat and T1 in WM between C2 and C5 vertebral levels
+sct_extract_metric -i mtr.nii.gz -f label_MTS/atlas -l 51 -vert 2:5 -vertfile label_MTS/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/MTR.csv -append 1
+sct_extract_metric -i mtsat.nii.gz -f label_MTS/atlas -l 51 -vert 2:5 -vertfile label_MTS/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/MTsat.csv -append 1
+sct_extract_metric -i t1map.nii.gz -f label_MTS/atlas -l 51 -vert 2:5 -vertfile label_MTS/template/PAM50_levels.nii.gz -o ${PATH_RESULTS}/T1.csv -append 1
+
 # # T2s
 # # ------------------------------------------------------------------------------
 # file_t2s="${SUBJECT}_T2star"
@@ -288,7 +287,7 @@ cd ..
 # ------------------------------------------------------------------------------
 FILES_TO_CHECK=(
   "anat/${SUBJECT}_T2w_bp-cspine_seg.nii.gz"
-  "anat/${SUBJECT}_T1_seg.nii.gz"
+  "anat/${SUBJECT}_T1w_seg.nii.gz"
 )
 for file in ${FILES_TO_CHECK[@]}; do
   if [[ ! -e $file ]]; then
